61382e9a48e68c0037747cf271b9dae69338020f
yrjt <yrjt@fresnou.org>
2023-01-18 12:51:36 -0800

    binary

---
+++ Chapter 1/datatext.js
@@ -0,0 +1,26 @@
+console.log(7);
+//This is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+
+console.log(10%2);
+
+console.log(0);
+
+console.log(3);
+//Floating Point Number
+console.log(3.14);
+console.log(100.045);
+
+//Strings
+//Characters. letters, symbols, spaces
+console.log("Hello world");
+console.log('Hello world!');
+console.log(`Hello world 123`);
+console.log('my'+'name');
+console.log("not"+" "+"today"+".");
+console.log("your age\n is 14");
+console.log("your age/n is 14");
+console.log('12+5=${12+5}');
