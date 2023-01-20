8ee4dce224fefbd27c9b33e944c51ab3cdac1fd4
yrjt <yrjt@fresnou.org>
2023-01-20 09:51:01 -0800

    binary

---
+++ Chapter 1/Operators.js
@@ -0,0 +1,29 @@
+//Unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+//Binary Operators use two values
+//Comparison Operators
+console.log(10 > 5);  //greater than
+
+console.log(10 < 5); //less than
+
+console.log(10 >= 5); //greater than or equal to
+
+console.log(10 <= 5); //less than or equal to
+
+console.log(10 == 5); //are they both equal? false
+
+console.log(10 == 10); //are they both equal? true
+
+console.log(10 != 5); //are they different? true
+
+console.log(10 != 10); //are they different? false
+
+console.log(15>15);
+
+console.log(15>=15)
+//Logical Operators
+
+//Ternary Operators use three values
--- Chapter 1/datatext.js
+++ Chapter 1/datatext.js
@@ -19,8 +19,17 @@ console.log(100.045);
 console.log("Hello world");
 console.log('Hello world!');
 console.log(`Hello world 123`);
+//Concatenation + combine strings
 console.log('my'+'name');
 console.log("not"+" "+"today"+".");
+console.log('My' + '' +'age'+'is'+ ' '+ 15 + '.');
+console.log('Your age is\n15.'); // \n means new line - dont put a space after n
 console.log("your age\n is 14");
 console.log("your age/n is 14");
-console.log('12+5=${12+5}');
+//Template literal- plug in a calculated value
+// use backticks '' next to the 1
+console.log(`15 divided by 10 = ${15/10}`);
+console.log(`12+5= ${12+5}`);
+console.log(`101 divided by 5 has the remainder of:${101%5}`);
+console.log(`Your age is ${14+3}`);
+console.log(`200 divided by 8 equals ${200/8}`);
