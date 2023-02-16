a63dec2bf7d2b127c6f1aebcbf3e24aa4e21bc79
yrjt <yrjt@fresnou.org>
2023-02-16 10:28:15 -0800

    commit

--- Conditionals/Input2.js
+++ Conditionals/Input2.js
@@ -13,6 +13,43 @@ let sum = equation - anotheranswer;{
 }
 //3
 let ok = Number(readLine.question("Pick a number\nYour number: "));
-let okay=number(readLine.question("Pick a second number\nYour number: "));
-let numerodos = uhhh - numerouno;{
-    console.log(numerodos)
+let okay = Number(readLine.question("Pick a second number\nYour number: "));
+let alr = ok * okay;
+{
+    console.log(alr)
+}
+//4
+let mhm = Number(readLine.question("Pick a number\nYour number: "));
+let mh = Number(readLine.question("Pick a second number\nYour number: "));
+let m = mhm / mh;
+{
+    console.log(m)
+}
+//5
+let a = Number(readLine.question("Pick a number\nYour number: "));
+let b = Number(readLine.question("Pick a second number\nYour number: "));
+let c = a % b;
+{
+    console.log(c)
+}
+//6
+let nameone = readLine.question("Whats Your First Name\nYour First Name: ");
+let nametwo = readLine.question("Whats Your Last Name\nYour Last Name: ");
+
+    console.log(` Greeting ${nametwo}, ${nameone} `);
+
+    //7
+let title = readLine.question("Whats Your Title\nYour Title: ");
+let lastnameagain = readLine.question("Whats Your Last Name\nYour Last Name: ");
+let suffix = readLine.question("Whats Your Suffix\nYour Suffix: ");
+
+console.log(` Greetings ${title}, ${lastnameagain}, ${suffix} `);
+
+//8
+let streetnumber = readLine.question("Whats Your Street Number\nYour Street Number: ");
+let streetname = readLine.question("Whats Your Street Name\nYour Street Name: ");
+let streettype = readLine.question("Whats Your Street Type\nYour Street Type: ");
+let city= readLine.question("Whats Your City\nYour City: ");
+let state = readLine.question("Whats Your State\nYour State: ");
+let zipcode = readLine.question("Whats Your Zip Code\nYour Zip Code: ");
+console.log(` Greetings ${zipcode}, ${state}, ${city} ${streettype}, ${streetname}, ${streetnumber} `);
