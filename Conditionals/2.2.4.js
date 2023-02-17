05ddddc6250b676741b27b42a075461c4cceb3bf
yrjt <yrjt@fresnou.org>
2023-02-17 10:28:53 -0800

    Started WildLoops

---
+++ Conditionals/2.2.4.js
@@ -0,0 +1,36 @@
+let a = 0;
+
+ while (a <= 25){
+
+ console.log(a);
+
+ a++;
+
+ }
+let b = 0;
+
+while (b <= 100){
+
+    console.log(b);
+
+    b++;
+
+}
+let c = -10;
+
+while (c <= 10){
+
+    console.log(c);
+
+    c++;
+
+}
+let d = 0;
+
+while (d >= -50){
+
+    console.log(c);
+
+    d--;
+
+}
