0561b7ea3163e8fe2c93fcbab0941f0533652e53
yrjt <yrjt@fresnou.org>
2023-02-24 10:25:31 -0800

    2.2.5 assignment

---
+++ Conditionals/2.2.5.js
@@ -0,0 +1,69 @@
+const readLine = require('readline-sync');
+//1
+for(let a = 0; a<=150; a++){
+console.log(a)
+}
+//2
+for(let b = 0; b<=40; b++){
+    console.log(b**2)
+}
+//3
+for(let c = 0; c<=30; c++){
+    console.log(c**3)
+}
+//4
+for(let d = 0; d<=700; d++){
+if(d % 5 ==0) {
+  console.log(d)
+}
+  }
+//5
+for(let e = 350; e<=1000; e++){
+if(e % 10 ==0) {
+  console.log(e)
+}
+}
+//6
+let sum = 0;
+for(let f = 0; f<=50; f++) {
+  sum += f;
+}
+console.log(sum);
+//7
+let total = 0;
+for(let g = 35; g<=70; g++) {
+    total += g;
+}
+console.log(total);
+//8
+let product =1;
+for(let h = 1; h<=15; h++) {
+    product *= h;
+    console.log(product);
+}
+console.log(product);
+//9
+let Product =1;
+for(let i = 10; i<=35; i++) {
+    Product *= i;
+    console.log(Product);
+}
+console.log(Product);
+//10
+for(let j = 0; j<=11; j++){
+console.log("Happy Holidays!")}
+//11
+let k = Number(readLine.question("Pick a number "));
+for(let j = 1; j<=k; j++){
+    console.log("Happy Holidays!");
+}
+//12
+let sums = 0;
+for(let l = 100; l<=200; l++) {
+    if(l % 2 =
+    ) {
+        console.log(d)
+    }
+    sum += l;
+}
+console.log(sums);
--- Conditionals/ForLoops.js
+++ Conditionals/ForLoops.js
@@ -1,4 +1,55 @@
 //For Loops
-for(let i = 0; i<=10){
 
+
+
+//for(let i = 0; i<=10; i++){
+//console.log("hi")
+//}
+
+//prints once as 0 then gradually increases till equal to 10
+//for every increase it reprints "hi"
+
+
+
+//for(let i = 0; i<=10; i++){
+//  console.log(i)
+//}
+//This will print out I instead of a word which means it basically pastes the number I represents as it changes till it reaches 10
+
+//for(let i = 0; i<1000; i++){
+//    console.log(i)
+//}
+//It will print every number starting from 0 until it is 1 off of 1000
+
+
+//for(let i = 100; i>=12; i--){
+  //  console.log(i)
+//}
+//as long as I is greater it continues to decrease until is equal to 12
+
+
+
+//for(let i = 100; i<=300; i++){
+    //if(i % 2 ==0) {
+      //  console.log(i)
+    //}
+  //  }
+//Prints out all even numbers between 100 and 300
+
+
+//let sum = 0;
+//for(let i = 100; i<=300; i++) {
+  //  sum += i;
+//}
+//console.log(sum);
+//only prints the sum of the value
+
+
+let sum = 0;
+for(let i = 100; i<=300; i++) {
+   if(i % 2 ==0) {
+       sum += i;
+   }
 }
+console.log(`sum: ${sum}`);
+//prints out sum if even
