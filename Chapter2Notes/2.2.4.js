1f34cd3c8d847323f386bbf5a123ce0b8cd460c9
yrjt <yrjt@fresnou.org>
2023-02-28 09:47:47 -0800

    commit

---
+++ Chapter2Notes/2.2.4.js
@@ -0,0 +1,71 @@
+//1
+let a = 0;
+
+while (a <= 25){
+
+    console.log(a);
+
+    a++;
+
+}
+//2
+let b = 0;
+
+while (b <= 100){
+
+    console.log(b);
+
+    b++;
+
+}
+//3
+let c = -10;
+
+while (c <= 10){
+
+    console.log(c);
+
+    c++;
+
+}
+//4
+let d = 0;
+
+while (d >= -50){
+
+    console.log(d);
+
+    d--;
+
+}
+//5
+let e = 0;
+
+if (e % 100 = 0){
+
+    console.log(e);
+
+    e++;
+    //Did not understand the questions beyond 5
+
+}
+//9
+let start = 0;
+
+while (start <= 100){
+
+    if(start < 10){
+
+        console.log(start);
+
+    }else if (start > 25){
+
+        console.log(start);
+
+    }
+
+    start++;
+
+}
+
+
--- Chapter2Notes/2.2.5.js
+++ Chapter2Notes/2.2.5.js
@@ -1,7 +1,7 @@
 const readLine = require('readline-sync');
 //1
 for(let a = 0; a<=150; a++){
-console.log(a)
+    console.log(a)
 }
 //2
 for(let b = 0; b<=40; b++){
@@ -13,20 +13,20 @@ for(let c = 0; c<=30; c++){
 }
 //4
 for(let d = 0; d<=700; d++){
-if(d % 5 ==0) {
-  console.log(d)
+    if(d % 5 ==0) {
+        console.log(d)
+    }
 }
-  }
 //5
 for(let e = 350; e<=1000; e++){
-if(e % 10 ==0) {
-  console.log(e)
-}
+    if(e % 10 ==0) {
+        console.log(e)
+    }
 }
 //6
 let sum = 0;
 for(let f = 0; f<=50; f++) {
-  sum += f;
+    sum += f;
 }
 console.log(sum);
 //7
@@ -51,7 +51,7 @@ for(let i = 10; i<=35; i++) {
 console.log(Product);
 //10
 for(let j = 0; j<=11; j++){
-console.log("Happy Holidays!")}
+    console.log("Happy Holidays!")}
 //11
 let k = Number(readLine.question("Pick a number "));
 for(let j = 1; j<=k; j++){
