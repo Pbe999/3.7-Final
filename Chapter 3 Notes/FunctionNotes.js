7ff6e729a0687241e871064bf7db087d79178ac4
yrjt <yrjt@fresnou.org>
2023-03-14 10:00:17 -0700

    commit

---
+++ Chapter 3 Notes/FunctionNotes.js
@@ -0,0 +1,42 @@
+//Functions Notes
+//Step 1: Define the function
+// function sayHello(){
+//     console.log('Hello');
+// }
+//Step 2: Call (or use) the function
+// sayHello();
+
+
+//This code lets be input any numbers I want when i go to use the function
+// function printMax(){
+//     let x = 6;
+//     let y = 29;
+//     if ( x > y){
+//         console.log(`The larger number is ${x}.`);
+//     } else {
+//         console.log(` The larger number is ${y}.`);
+//     }
+// }
+// printMax();
+
+
+// function printMax(x, y){
+//          if ( x > y){
+//         console.log(`The larger number is ${x}.`);
+//     } else {
+//         console.log(` The larger number is ${y}.`);
+//     }
+// }
+//
+// printMax(16, 9); //first number will be x second will be y automatically
+// printMax( 2, 75);
+// printMax(5, 5);
+
+
+
+//uses addition and lets me set the values at the end
+function sum(num1, num2){
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+}
+sum(7, 5);
+sum(4,1094);
