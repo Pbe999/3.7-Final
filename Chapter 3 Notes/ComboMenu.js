e010d2552c94444c14aba30d78b017646de1da1f
yrjt <yrjt@fresnou.org>
2023-05-01 09:35:34 -0700

    initial commit

--- Chapter 3 Notes/ComboMenu.js
+++ Chapter 3 Notes/ComboMenu.js
@@ -1,54 +1,233 @@
 const readline = require('readline-sync');
 
-let sandwich = readline.question("What type of sandwich would you like(chicken, beef, or tofu):");
+
 
 let sandwichPrices = {
+
     chicken: 5.25,
-    beef:6.25,
-    tofu:5.67
-};
-if(sandwichPrices.hasOwnProperty(sandwich)){
-    console.log(`You selected a `+ sandwich + " sandwich for $" + sandwichPrices [sandwich] +`.`);
-} else{
-    console.log("Sorry we dont have that sandwich on the menu");
-}
 
-let wantbeverage = readline.question("Would you like a beverage? yes or no:");
-console.log(`wantbeverage: ${wantbeverage}`);
+    beef: 6.25,
+
+    tofu: 5.67
+
+};
+
+
+
 let beveragePrices = {
+
     small: 1.00,
+
     medium: 1.75,
+
     large: 2.75
+
 };
 
-let beverageCost = 0;
 
-if (wantbeverage == "yes"){
-    let beverageSize = readline.question("What size beverage would you like? small, medium, or large :");
 
-    if (beveragePrices.hasOwnProperty(beverageSize)) {
-        beverageCost = beveragePrices[beverageSize];
-        console.log(`You selected a` + beverageSize + ' beverage for $' + beverageCost + `.`)
-    } else {
-        console.log("Sorry, we don't have that beverage size on the menu.");
+let friesPrices = {
+
+    small: 1.00,
+
+    medium: 1.50,
+
+    large: 2.00
+
+};
+
+
+
+while (true) {
+
+    let sandwich = readline.question("What type of sandwich would you like (chicken, beef, or tofu)? ");
+
+
+
+    if (!sandwichPrices.hasOwnProperty(sandwich)) {
+
+        console.log("Invalid sandwich selection. Please choose from chicken, beef, or tofu.");
+
+        continue;
+
     }
-}
-let totalCost = sandwichPrices[sandwich] + beverageCost;
-if (wantbeverage === "yes"){
-    console.log(`Your total cost is $` + totalCost.toFixed(2) + '.');
-}else{
-    console.log("your total cost is $" + sandwichPrices[sandwich].toFixed(2) + '.');
-}
 
-let wantFries = readline.question("Would you like fries(yes or no):");
-console.log(`wantFries: ${wantFries}`);
-let friesPrices ={
-    small:1.00,
-    medium:1.50,
-    large:2.00
-};
-let friesSize;
-let friesCost = 0;
-if(wantFries == "yes");{
-friesSize = readline.question(" What size fries woudl you like? (small, medium, or large:")
-}
+
+
+    let wantBeverage = readline.question("Would you like a beverage? (yes or no): ");
+
+
+
+    if (wantBeverage !== "yes" && wantBeverage !== "no") {
+
+        console.log("Invalid selection. Please enter yes or no.");
+
+        continue;
+
+    }
+
+
+
+    let beverageCost = 0;
+
+    let beverageSize = "none";
+
+
+
+    if (wantBeverage === "yes") {
+
+        while (true) {
+
+            beverageSize = readline.question("What size beverage would you like? (small, medium, or large): ");
+
+
+
+            if (!beveragePrices.hasOwnProperty(beverageSize)) {
+
+                console.log("Invalid beverage size. Please choose from small, medium, or large.");
+
+                continue;
+
+            }
+
+
+
+            beverageCost = beveragePrices[beverageSize];
+
+            break;
+
+        }
+
+    }
+
+
+
+    let wantFries = readline.question("Would you like fries? (yes or no): ");
+
+
+
+    if (wantFries !== "yes" && wantFries !== "no") {
+
+        console.log("Invalid selection. Please enter yes or no.");
+
+        continue;
+
+    }
+
+
+
+    let friesSize = "none";
+
+    let friesCost = 0;
+
+
+
+    if (wantFries === "yes") {
+
+        while (true) {
+
+            friesSize = readline.question("What size fries would you like? (small, medium, or large): ");
+
+
+
+            if (!friesPrices.hasOwnProperty(friesSize)) {
+
+                console.log("Invalid fries size. Please choose from small, medium, or large.");
+
+                continue;
+
+            }
+
+
+
+            friesCost = friesPrices[friesSize];
+
+
+
+            if (friesSize === "small") {
+
+                let megaSize = readline.question("Would you like to mega-size your fries? (yes or no): ");
+
+
+
+                if (megaSize === "yes") {
+
+                    friesSize = "large";
+
+                    friesCost = friesPrices[friesSize];
+
+                }
+
+            }
+
+
+
+            break;
+
+        }
+
+    }
+
+
+
+    let wantKetchupPackets = readline.question("How many ketchup packets would you like? (enter a positive integer): ");
+
+
+
+    if (!/^\d+$/.test(wantKetchupPackets)) {
+
+        console.log("Invalid input. Please enter a positive integer.");
+
+        continue;
+
+    }
+
+
+
+    let ketchupPackets = parseInt(wantKetchupPackets);
+
+    let ketchupCost = ketchupPackets * 0.25;
+
+
+
+    let totalCost = sandwichPrices[sandwich] + beverageCost + friesCost + ketchupCost;
+
+
+
+    if (sandwichPrices.hasOwnProperty(sandwich) && wantFries === "yes" && wantBeverage === "yes") {
+
+        console.log("Combo sale: You get $1.00 off!");
+
+        totalCost -= 1.00;
+
+    }
+
+
+
+    console.log("----Order Summary----");
+
+    let subtotal = totalCost + ketchupCost;
+
+
+
+    let tax = subtotal * 0.07;
+
+
+
+    let total = subtotal + tax;
+
+
+
+
+
+
+
+    console.log(`Subtotal: $${subtotal.toFixed(2)}`);
+
+
+
+    console.log(`Tax (7%): $${tax.toFixed(2)}`);
+
+
+
+    console.log(`Total: $${total.toFixed(2)}`); }
